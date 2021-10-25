ebfb6c549563711526f7562a115a27da6913bbdb
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-10-25 11:45:37 +0300

    Add files via upload

---
+++ src/hooks/useJsonFetch.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+export default function useJsonFetch(url) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios(url)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((e) => {
+        setError(e);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setError(false);
+      })
+  }, [url]);
+  return [data, isLoading, error];
+}
